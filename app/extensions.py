a735814de021e3247e49ad5e4f15788623db793b
mpace <mt.pace0@gmail.com>
2025-03-22 21:15:15 -0700

    typo

--- app/extensions.py
+++ app/extensions.py
@@ -3,7 +3,7 @@ Extensions module.
 Each extension is initialized in the app factory located in app/__init__.py
 """
 import pymongo
-from flask import gurl_for, flash, request
+from flask import url_for, flash, request
 from functools import wraps
 import os
 import logging
